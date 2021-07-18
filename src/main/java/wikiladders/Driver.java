6412175ed3f83597f77dcb2d4bf218d31dfab90c
fcseidl <fcseidl@umich.edu>
2021-07-18 13:09:46 -0400

    created driver

---
+++ src/main/java/wikiladders/Driver.java
@@ -0,0 +1,15 @@
+package wikiladders;
+
+/**
+ * Short driver program for wikiladders package.
+ * 
+ * @author fcseidl
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		SimoneScrape.scrape();
+	} // main()
+
+} // Driver
--- src/main/java/wikiladders/SimoneScrape.java
+++ src/main/java/wikiladders/SimoneScrape.java
@@ -2,7 +2,7 @@ package wikiladders;
 
 import java.io.IOException;
 
-import wikiladders.WikiNode;
+//import wikiladders.WikiNode;
 
 /**
  * Get the HTML for Simone Weil's (the only great spirit of our time) Wikipedia
@@ -10,18 +10,18 @@ import wikiladders.WikiNode;
  */
 public class SimoneScrape {
 
-    public static void main(String[] args) {
+    public static void scrape() {
 
         try {
         	WikiNode node = new WikiNode("Simone_Weil");
         	for (String title : node.getChildren()) {
 				System.out.println(title);
-			}
+			} // for
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } // try-catch
 
-    } // main()
+    } // scrape()
 
 } // SimoneScrape
